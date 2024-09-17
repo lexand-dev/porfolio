08800d73e60827fb062605dad3443348a14a7544
alexander flores <alexfloresrayme@gmail.com>
2024-09-17 10:41:24 -0500

    refactor:files, styles, components

--- src/components/react/GlowSection.tsx
+++ src/components/react/GlowSection.tsx
@@ -1,6 +1,11 @@
 import { Glow, GlowCapture } from "@codaworks/react-glow";
 
-const GlowSection = () => {
+interface GlowSectionProps {
+  title: string;
+  description: string;
+}
+
+const GlowSection: React.FC<GlowSectionProps> = ({ title, description }) => {
   return (
     <GlowCapture className="flex justify-start py-16 w-full">
       <Glow color="purple">
@@ -15,25 +20,17 @@ const GlowSection = () => {
           md:py-12
           text-left 
           dark:border-neutral-800
-          border-slate-200 border
-          border-white/30
+          border-slate-2 border
           glow:text-glow/50 
           glow:bg-green-500/20
           glow:border-green-500/40
           glow:border-2
+          
           transition-colors"
         >
-          <h3 className="text-xl py-2">Modern Web Stack</h3>
+          <h3 className="text-xl py-2">{title}</h3>
           <div className="text-base">
-            <p className="pb-1">
-              Experienced in functional and OOP languages, I mainly prefer to
-              use ES6 and ReactJS on my frontend projects.{" "}
-            </p>
-            <p>
-              Despite being involved in Bootstrap and Material UI projects in
-              the past, my current preference has been towards TailwindCSS in
-              projects.
-            </p>
+            <p className="pb-1">{description}</p>
           </div>
         </article>
       </Glow>
