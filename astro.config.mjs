528933a371626c78e4cbe3641a8f1b6ef86a9283
alexander flores <alexfloresrayme@gmail.com>
2025-08-04 21:16:00 -0500

    fix: add Vercel adapter configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -19,5 +21,8 @@ export default defineConfig({
       en: "es"
     }
   },
-  output: "server"
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
